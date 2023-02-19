7b56221bbef80292adc2204897c7f579e23dc845
Melih Basayigit <melihbasayigit@gmail.com>
2023-02-19 11:19:15 +0300

    init

---
+++ src/main/java/com/melomanya/groupchatapp/GroupChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.melomanya.groupchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melomanya/groupchatapp/GroupChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melomanya.groupchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
